f333c23b29461e307924991c60944dc5d6f0f3f6
Virtual-Truck <juangarros2005@gmail.com>
2025-01-12 20:16:03 +0100

    espero

--- entorno_cliente/prueba/codigo_DWEC/tareas/tarea4.3/fuente/correccion.js
+++ entorno_cliente/prueba/codigo_DWEC/tareas/tarea4.3/fuente/correccion.js
@@ -42,7 +42,6 @@ class Direccion {
     /**
     * ## Constructor de la clase Dirección<br>
     * 
-    * Inicializacion: <br>
     * 
     * Inicializa una nueva instancia de la clase Dirección.<br>
     * 
