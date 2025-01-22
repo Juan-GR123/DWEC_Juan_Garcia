cb2c7c38720449fe75501eb3d555f2bf9eedb1ec
Juan-GR123 <juangarros2005@gmail.com>
2025-01-22 19:46:11 +0100

    nuevo

--- tareas/DWEC_Proyecto_SGAEA/js/correccion.js
+++ tareas/DWEC_Proyecto_SGAEA/js/correccion.js
@@ -1,5 +1,5 @@
 //Este proyecto esta disponible en mi github
-//El enlace es: https://github.com/Virtual-Truck/git_cliente/blob/main/entorno_cliente/prueba/codigo_DWEC/tareas/DWEC_Proyecto_SGAEA/correccion.js
+//El enlace es: https://github.com/Juan-GR123/DWEC_Juan_Garcia/blob/main/entorno_cliente/prueba/codigo_DWEC/tareas/DWEC_Proyecto_SGAEA/correccion.js
 
 
 //Todas las salidas serán por consola y todas las entradas por teclado
